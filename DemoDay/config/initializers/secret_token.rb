a97ab35771fd10c717476cc81948aaa51781d9c3
Erica Mazza <erica.mazza1@gmail.com>
2012-11-08 11:49:29 -0600

    rails new

---
+++ DemoDay/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoDay::Application.config.secret_token = 'a1203c04ed43aaf9793183e58450b0e08a4257527b4fdf910276bd9f361f9ab65f34f5c7a15c3666bd56ab340e358e60980d4d5f858fe95684126da539a03419'
